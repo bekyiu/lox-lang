06c625d9111fa9f5573ae3f9704340710b9b3dd8
bekyiu <651731457@qq.com>
2023-03-01 20:43:30 +0800

    fix: 扩展lox value, 支持更多的表达式

---
+++ clox/header/object.h
@@ -0,0 +1,39 @@
+//
+// Created by bekyiu on 2023/3/1.
+//
+
+#ifndef CLOX_OBJECT_H
+#define CLOX_OBJECT_H
+
+#include "common.h"
+#include "value.h"
+
+// 在堆上分配内存的lox value类型
+typedef enum {
+    OBJ_STRING,
+} ObjType;
+
+// 所有lox对象的父类
+struct Obj {
+    ObjType type;
+};
+
+struct ObjString {
+    Obj obj;
+    int length;
+    char *chars;
+};
+
+#define OBJ_TYPE(value)        (AS_OBJ(value)->type)
+#define IS_STRING(value)       isObjType(value, OBJ_STRING)
+
+#define AS_STRING(value)       ((ObjString*)AS_OBJ(value))
+#define AS_CSTRING(value)      (((ObjString*)AS_OBJ(value))->chars)
+
+static inline bool isObjType(Value value, ObjType type) {
+    return IS_OBJ(value) && AS_OBJ(value)->type == type;
+}
+
+ObjString* copyString(const char* chars, int length);
+
+#endif //CLOX_OBJECT_H
--- clox/header/value.h
+++ clox/header/value.h
@@ -3,7 +3,43 @@
 
 #include "common.h"
 
-typedef double Value;
+typedef struct Obj Obj;
+typedef struct ObjString ObjString;
+
+typedef enum {
+    VAL_BOOL,
+    VAL_NIL,
+    VAL_NUMBER,
+    VAL_OBJ,
+} ValueType;
+
+// lox value 到 c变量的映射
+typedef struct {
+    ValueType type;
+    union {
+        bool boolean;
+        double number;
+        // 指向堆上的一个对象
+        Obj* obj;
+    } as;
+} Value;
+
+bool valuesEqual(Value a, Value b);
+
+#define BOOL_VAL(value)   ((Value){VAL_BOOL, {.boolean = (value)}})
+#define NIL_VAL           ((Value){VAL_NIL, {.number = 0}})
+#define NUMBER_VAL(value) ((Value){VAL_NUMBER, {.number = (value)}})
+#define OBJ_VAL(object)   ((Value){VAL_OBJ, {.obj = (Obj*)(object)}})
+
+#define AS_BOOL(value)    ((value).as.boolean)
+#define AS_NUMBER(value)  ((value).as.number)
+#define AS_OBJ(value)     ((value).as.obj)
+
+
+#define IS_BOOL(value)    ((value).type == VAL_BOOL)
+#define IS_NIL(value)     ((value).type == VAL_NIL)
+#define IS_NUMBER(value)  ((value).type == VAL_NUMBER)
+#define IS_OBJ(value)     ((value).type == VAL_OBJ)
 
 // 常量池 存储程序中以字面量形式出现的值
 typedef struct {
